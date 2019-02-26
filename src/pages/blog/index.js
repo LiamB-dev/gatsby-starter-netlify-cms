2fc77d6daa6ea7ce82219f75483b6dda8caa74a3
Liamb-immalle <liam.brangers@immalle.be>
2019-02-26 19:19:20 +0100

    remove PleinDag

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 
 import Layout from '../../components/Layout'
 import BlogRoll from '../../components/BlogRoll'
-import PleinDag from '../../components/PleinDag'
 
 export default class BlogIndexPage extends React.Component {
   render() {
@@ -12,44 +11,9 @@ export default class BlogIndexPage extends React.Component {
         <section className="section">
           <div className="container">
             <div className="content">
-            <div>
-                
-                <h1
-                  className="has-text-weight-bold is-size-1"
-                  style={{
-                    boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-                    backgroundColor: '#f40',
-                    color: 'white',
-                    padding: '1rem',
-                  }}
-                >
-                  Algemene info
-                </h1>
-              </div>
             </div>
               <BlogRoll />
             </div>    
-        </section>
-        <section className="section">
-            <div className="container">
-            <div className="content">
-            <div>
-                
-                <h1
-                  className="has-text-weight-bold is-size-1"
-                  style={{
-                    boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-                    backgroundColor: '#f40',
-                    color: 'white',
-                    padding: '1rem',
-                  }}
-                >
-                  Een dag op het speelplein
-                </h1>
-              </div>
-            </div>
-              <PleinDag />
-            </div>
         </section>
       </Layout>
     )
