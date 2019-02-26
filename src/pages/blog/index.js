bed2d4cc78e2add2d059c34d711a7b0866d75f39
Liamb-immalle <liam.brangers@immalle.be>
2019-02-26 18:42:28 +0100

    Update index.js

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -22,7 +22,26 @@ export default class BlogIndexPage extends React.Component {
                     padding: '1rem',
                   }}
                 >
-                  Info
+                  Algemene info
+                </h1>
+              </div>
+            </div>
+              <BlogRoll />
+            </div>
+            <div className="container">
+            <div className="content">
+            <div>
+                
+                <h1
+                  className="has-text-weight-bold is-size-1"
+                  style={{
+                    boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
+                    backgroundColor: '#f40',
+                    color: 'white',
+                    padding: '1rem',
+                  }}
+                >
+                  Een dag op het speelplein
                 </h1>
               </div>
             </div>
