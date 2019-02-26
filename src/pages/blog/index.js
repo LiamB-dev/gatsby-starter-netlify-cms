8bc1e7d5de306bf3af64c580617548d72f549dd7
Liamb-immalle <liam.brangers@immalle.be>
2019-02-26 18:39:53 +0100

    Update index.js

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -11,7 +11,20 @@ export default class BlogIndexPage extends React.Component {
         <section className="section">
           <div className="container">
             <div className="content">
-            
+            <div>
+                
+                <h1
+                  className="has-text-weight-bold is-size-1"
+                  style={{
+                    boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
+                    backgroundColor: '#f40',
+                    color: 'white',
+                    padding: '1rem',
+                  }}
+                >
+                  Info
+                </h1>
+              </div>
             </div>
               <BlogRoll />
             </div>
