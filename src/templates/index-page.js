db2775f18defb0aee83e50e73da64d07ac341e04
Liamb-immalle <liam.brangers@immalle.be>
2019-03-16 12:51:56 +0100

    Update index-page.js

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -73,22 +73,6 @@ export const IndexPageTemplate = ({
               </div>
               <Features gridItems={intro.blurbs} />
               <div className="columns">
-              <div className="column is-12 has-text-centered">
-                <Link className="btn" to="/products">
-                  See all products
-                </Link>
-                </div>
-              </div>
-              <div className="column is-12">
-                  <h3 className="has-text-weight-semibold is-size-2">
-                    Info
-                  </h3>
-                  <BlogRoll />
-                  <div className="column is-12 has-text-centered">
-                  <Link className="btn" to="/blog">
-                    Meer lezen
-                  </Link>
-                  </div>
               </div>
               </div>
           </div>
