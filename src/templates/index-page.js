65ecc58a7ddf32074f26f1f9b7327091c0c96725
Liamb-immalle <liam.brangers@immalle.be>
2019-04-11 20:20:01 +0200

    Update index-page.js

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -62,25 +62,21 @@ export const IndexPageTemplate = ({
                     <h1>{heading1}</h1>
                     {body1}
                   </div>
-                  <div className="is-2"></div>
                   <div className="is-4">
                     <h1>{heading2}</h1>
                     {body2}
                   </div>
                 </div>
-              <div className="content">
-                <div className="tile">
+                <div className="tile is-offset-1">
                   <div className="is-4">
                     <h1>{heading3}</h1>
                     {body3}
                   </div>
-                  <div className="is-2"></div>
                   <div className="is-4">
                     <h1>{heading4}</h1>
                     {body4}
                   </div>                  
                 </div>
-              </div>
               </div>
             </div>
           </div>
