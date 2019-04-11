1df941e4c4882611c7316b7773a9b1d77c94c8e0
Liamb-immalle <liam.brangers@immalle.be>
2019-04-11 19:48:28 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import logo from '../img/logo.svg'
 import facebook from '../img/social/facebook.svg'
 
 const Footer = class extends React.Component {
@@ -9,11 +8,7 @@ const Footer = class extends React.Component {
     return (
       <footer className="footer has-background-black has-text-white-ter">
         <div className="content has-text-centered">
-          <img
-            src={logo}
-            alt="Speelplein Wuustwezel"
-            style={{ width: '14em', height: '10em' }}
-          />
+          <h1>Speelplein Wuustwezel</h1>
         </div>
         <div className="content has-text-centered has-background-black has-text-white-ter">
           <div className="container has-background-black has-text-white-ter">
