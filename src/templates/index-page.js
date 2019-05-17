b9d97808dd354f992d142216ade744938a4c83e1
Liamb-immalle <liam.brangers@immalle.be>
2019-05-17 11:10:09 +0200

    Update index-page.js

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {graphql } from 'gatsby'
+import { graphql } from 'gatsby'
 
 import Layout from '../components/Layout'
 
@@ -14,78 +14,77 @@ export const IndexPageTemplate = ({
   heading3,
   body3,
   heading4,
-  body4,  
+  body4,
 }) => (
     <div>
-        <div
-          className="full-width-image margin-top-0"
-          style={{
-            backgroundImage: `url(${
-              !!image.childImageSharp
-                ? image.childImageSharp.fluid.src
-                : image
-            })`,
-            backgroundPosition: `top left`,
-            backgroundAttachment: `fixed`,
-          }}
-    >
-  <div style={{
-        display: 'flex',
-        height: '150px',
-        lineHeight: '1',
-        justifyContent: 'space-around',
-        alignItems: 'left',
-        flexDirection: 'column' }}>
-      <h1
-        className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
+      <div
+        className="full-width-image margin-top-0"
         style={{
-          boxShadow: 'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-          backgroundColor: 'rgb(255, 68, 0)',
-          color: 'white',
-          lineHeight: '1',
-          padding: '0.25em'
+          backgroundImage: `url(${
+            !!image.childImageSharp
+              ? image.childImageSharp.fluid.src
+              : image
+            })`,
+          backgroundPosition: `top left`,
+          backgroundAttachment: `fixed`,
         }}
       >
-        {title}
-      </h1>
+        <div style={{
+          display: 'flex',
+          height: '150px',
+          lineHeight: '1',
+          justifyContent: 'space-around',
+          alignItems: 'left',
+          flexDirection: 'column'
+        }}>
+          <h1
+            className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
+            style={{
+              boxShadow: 'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
+              backgroundColor: 'rgb(255, 68, 0)',
+              color: 'white',
+              lineHeight: '1',
+              padding: '0.25em'
+            }}
+          >
+            {title}
+          </h1>
+        </div>
       </div>
-    </div>
-  <section className="section section--gradient">
-    <div className="container">
-      <div className="section">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <div className="content">
-              <div className="content">
-                <div className="tile">
-                  <div className="is-4">
-                    <h1>{heading1}</h1>
-                    {body1}
+      <section className="section section--gradient">
+        <div className="container">
+          <div className="section">
+            <div className="columns">
+              <div className="column is-10 is-offset-1">
+                <div className="content">
+                  <div className="tile">
+                    <div className="is-4">
+                      <h1>{heading1}</h1>
+                      {body1}
+                    </div>
+                    <div className="is-4">
+                      <h1>{heading2}</h1>
+                      {body2}
+                    </div>
                   </div>
-                  <div className="is-4">
-                    <h1>{heading2}</h1>
-                    {body2}
+                  <div className="tile is-offset-1">
+                    <div className="is-4">
+                      <h1>{heading3}</h1>
+                      {body3}
+                    </div>
+                    <div className="is-4">
+                      <h1>{heading4}</h1>
+                      {body4}
+                    </div>
                   </div>
-                </div>
-                <div className="tile is-offset-1">
-                  <div className="is-4">
-                    <h1>{heading3}</h1>
-                    {body3}
-                  </div>
-                  <div className="is-4">
-                    <h1>{heading4}</h1>
-                    {body4}
-                  </div>                  
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </section>
     </div>
-  </section>
-  </div>
-)
+  )
 
 IndexPageTemplate.propTypes = {
   image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
