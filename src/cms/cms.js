507a5e8a106d9f14461c977852ad36e1fb3d3c10
LiamB-dev <liam.brangers@gmail.com>
2019-11-15 11:21:35 +0100

    Update cms.js

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,12 +1,10 @@
 import CMS from 'netlify-cms'
 
-import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('contact', IndexPagePreview)
-CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
 CMS.registerPreviewTemplate('animatoren', BlogPostPreview)
 CMS.registerPreviewTemplate('specials', BlogPostPreview)
