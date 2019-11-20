e190d0a496231067bf55b10f77b36f2631334549
LiamB-dev <liam.brangers@gmail.com>
2019-11-20 11:21:49 +0100

    Update contact-page.js

--- src/components/contact-page.js
+++ src/components/contact-page.js
@@ -2,10 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 
-import Layout from 'Layout'
 
 export const ContactPageTemplate = ({
-  title,
   body
 
 }) => (
