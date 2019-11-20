952a14bd7327dfbfe4a9f7d9658894a432d881f9
LiamB-dev <liam.brangers@gmail.com>
2019-11-20 14:41:53 +0100

    Update index.js

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -41,7 +41,6 @@ export default class Index extends React.Component {
           <div className="container">
             <div className="content">
               <h1>Contact</h1>
-              <ContactPage />
               <form
                 name="contact"
                 method="post"
