4cff9eaedcabd0e58ff9d2fda8c33b117e1009b3
LiamB-dev <liam.brangers@gmail.com>
2019-11-20 15:14:48 +0100

    contactroll

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { navigate } from 'gatsby-link'
 import Layout from '../../components/Layout'
-import ContactPageTemplate from '../../components/contact-page'
 
 function encode(data) {
   return Object.keys(data)
